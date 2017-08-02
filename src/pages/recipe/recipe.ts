556a03a197e490b3433d982d21b934a770c26283
joshgressman <joshgressman@gmail.com>
2017-08-02 16:00:02 -0500

    added route to recipe with navParams recipe object

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Recipe } from '../../models/recipe';
+
 
 
 @Component({
@@ -8,8 +10,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'recipe.html',
 })
 export class RecipePage {
-
+   recipe: Recipe;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   this.recipe = {title: this.navParams.get('title'), description: this.navParams.get('title'), difficulty: this.navParams.get('difficulty'), ingredients: this.navParams.get('ingredients') }
+   console.log('the recipe in params', this.recipe);
   }
 
   ionViewDidLoad() {
--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { EditRecipePage } from '../edit-recipe/edit-recipe';
+import { RecipePage } from '../recipe/recipe';
 
 import { RecipeService } from '../../services/recipe';
 import { Recipe } from '../../models/recipe';
@@ -13,14 +14,18 @@ export class RecipesPage {
 
   recipes: Recipe[] = [];
 
-  constructor(private navCtrl: NavController, private recipeService: RecipeService){}
+  constructor(private navCtrl: NavController, private recipeService: RecipeService, private navParams: NavParams){}
 
   //Navigate to edit recipe page
   onNewRecipe(){
    this.navCtrl.push(EditRecipePage, {mode: 'New'});
   }
 
-  onLoadRecipe(){}
+  onLoadRecipe(i){
+    let recipe = this.recipes[i];
+   this.navCtrl.push(RecipePage, recipe);
+    
+  }
 
   //Loads recipes once view is entered after creating a recipe in the recip-edit comp
   ionViewWillEnter(){
