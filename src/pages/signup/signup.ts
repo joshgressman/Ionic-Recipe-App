35fc9da726246090bf0505a3899bec91a9f7efec
joshgressman <joshgressman@gmail.com>
2017-08-22 16:09:48 -0500

    added side menu

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SignupPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+
+
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
